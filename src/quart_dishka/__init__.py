298e8ae08dff204a526268536348a560e674ea15
hrimov <andrew.hrimov@gmail.com>
2026-08-05 12:30:01 +0200

    Add release workflow

--- src/quart_dishka/__init__.py
+++ src/quart_dishka/__init__.py
@@ -1,8 +1,13 @@
 __all__ = [
     'QuartDishka',
     'QuartProvider',
+    '__version__',
     'inject',
 ]
 
+from importlib.metadata import version
+
 from .extension import QuartDishka, inject
 from .provider import QuartProvider
+
+__version__ = version('quart-dishka')
